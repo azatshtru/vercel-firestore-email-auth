e6671ac3b5fb025e7f3f10de7d82768839e53e5f
azatshtru <adeveloper121@gmail.com>
2023-12-14 01:47:32 +0530

    Only subtract from bucket if wrong code entered

--- api/authcode.py
+++ api/authcode.py
@@ -32,14 +32,6 @@ class handler(BaseHTTPRequestHandler):
             self.wfile.write(json.dumps({"error":"ERRN0F", "description":"no associated auth entry found. (n0f)"}).encode('utf-8'))
             return
 
-        #check if wrong authcode entered multiple times in a row
-        bucket = doc['bucket']
-        if bucket < 1:
-            f.delete_document(id_token=token_id, db_name='dash-12112', collection_path=['serverauth'], document_id=f'{reciever_email}')
-            self.wfile.write(json.dumps({"error":"ERRIXT", "description":"recieved code was incorrect multiple times in a row. (ixt)"}).encode('utf-8'))
-            return
-        f.update_document(id_token=token_id, db_name='dash-12112', collection_path=['serverauth'], document_id=f'{reciever_email}', data={ 'bucket': int(bucket)-1 })
-
         #check if email code is expired
         if (time.time() - doc['timestamp']) > (AUTHCODE_EXPIRE_MINUTES+AUTHCODE_EXPIRE_MINUTES+2)*60:
             self.wfile.write(json.dumps({"error":"ERRE4E", "description":"recieved code was expired (e4e)"}).encode('utf-8'))
@@ -50,6 +42,13 @@ class handler(BaseHTTPRequestHandler):
             user_token = f.create_custom_token(reciever_email)
             self.wfile.write(json.dumps({"token":user_token}).encode('utf-8'))
         else:
+            #check if wrong authcode entered multiple times in a row
+            bucket = doc['bucket']
+            if bucket < 1:
+                f.delete_document(id_token=token_id, db_name='dash-12112', collection_path=['serverauth'], document_id=f'{reciever_email}')
+                self.wfile.write(json.dumps({"error":"ERRIXT", "description":"recieved code was incorrect multiple times in a row. (ixt)"}).encode('utf-8'))
+                return
+            f.update_document(id_token=token_id, db_name='dash-12112', collection_path=['serverauth'], document_id=f'{reciever_email}', data={ 'bucket': int(bucket)-1 })
             self.wfile.write(json.dumps({"error":"ERRI7T", "description":"recieved code was incorrect (i7t)"}).encode('utf-8'))
 
         return
