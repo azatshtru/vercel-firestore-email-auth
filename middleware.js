f9b0420f9cd5e87c9de2145b1399e62ea8218281
azatshtru <adeveloper121@gmail.com>
2023-12-13 12:28:22 +0530

    middleware testing

---
+++ middleware.js
@@ -0,0 +1,29 @@
+import { ipAddress, next } from '@vercel/edge'
+import { Ratelimit } from '@upstash/ratelimit'
+import { Redis } from "@upstash/redis";
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  // 5 requests from the same IP in 10 seconds
+  limiter: Ratelimit.slidingWindow(5, '10 s'),
+})
+
+// Define which routes you want to rate limit
+export const config = {
+  matcher: '/api/:path*',
+}
+
+export default async function middleware(request) {
+    console.log('works!');
+    // You could alternatively limit based on user ID or similar
+    const ip = ipAddress(request) || '127.0.0.1'
+    const { success, pending, limit, reset, remaining } = await ratelimit.limit(
+        ip
+    )
+
+    console.log('success?', success);
+
+    return success
+        ? next()
+        : Response.redirect(new URL('/blocked.html', request.url))
+}
